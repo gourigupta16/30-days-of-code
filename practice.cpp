3ea2ac1705369a6c5644a625b89ab84dc13b728c
gourigupta16 <104294178+gourigupta16@users.noreply.github.com>
2022-05-20 18:29:13 +0530

    Create practice.cpp

---
+++ practice.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#define ll long long int
+using namespace std;
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int X,Y;
+        cin>>X>>Y;
+        cout<<((10*X)+(90*Y))<<endl;
+    }
+    return 0;
+}
